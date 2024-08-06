6dcdd4bf9f0471f110beba02c199029dfb7a3dfb
dghosh17 <144285611+dghosh17@users.noreply.github.com>
2024-08-06 11:22:01 -0700

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -3,21 +3,19 @@ showSlides();
 
 function showSlides() {
     let slides = document.querySelectorAll(".mySlides");
-    
-    // Hide all slides
+
     slides.forEach(slide => {
         slide.style.display = "none";
+        slide.classList.remove("fade");
     });
-    
-    // Increment slideIndex and loop back if necessary
+
     slideIndex++;
     if (slideIndex > slides.length) { 
         slideIndex = 1;
     }
-    
-    // Show the current slide
+
     slides[slideIndex - 1].style.display = "block";
-    
-    // Change slide every 3 seconds
+    slides[slideIndex - 1].classList.add("fade");
+
     setTimeout(showSlides, 3000);
 }
