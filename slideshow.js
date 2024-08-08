e49e7593c5663fd797fb2147549e604a0ae96735
dghosh17 <144285611+dghosh17@users.noreply.github.com>
2024-08-07 23:10:52 -0700

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -4,18 +4,22 @@ showSlides();
 function showSlides() {
     let slides = document.querySelectorAll(".mySlides");
 
+    // Hide all slides and remove fade class
     slides.forEach(slide => {
         slide.style.display = "none";
         slide.classList.remove("fade");
-    }); 
+    });
 
+    // Increment slide index
     slideIndex++;
     if (slideIndex > slides.length) { 
         slideIndex = 1;
     }
 
+    // Show the current slide and add fade class
     slides[slideIndex - 1].style.display = "block";
     slides[slideIndex - 1].classList.add("fade");
 
+    // Call showSlides every 3 seconds
     setTimeout(showSlides, 3000);
 }
